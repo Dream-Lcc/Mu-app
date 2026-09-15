95fdeaf66656bd8ec7d025db46f4313375d1115d
Dream-Lcc <1454919512@qq.com>
2026-09-15 16:07:06 +0800

    创建首页页面

---
+++ app/src/main/java/com/campus/mu/screen/HomeScreen.kt
@@ -0,0 +1,27 @@
+package com.campus.mu.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "首页", style = MaterialTheme.typography.titleLarge)
+        Text(text = "沐APP主页", style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
