c46b43b01397b1ccdc826c1df5f4d6f9867a64e7
Dream-Lcc <1454919512@qq.com>
2026-09-15 16:13:31 +0800

    创建个人中心页面

---
+++ app/src/main/java/com/campus/mu/screen/MineScreen.kt
@@ -0,0 +1,27 @@
+package com.campus.mu.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MineScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "我的", style = MaterialTheme.typography.titleLarge)
+        Text(text = "个人中心页面", style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
